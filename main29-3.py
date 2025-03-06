3c62d8c5ca29c6061f2a68928145808382c6c428
모두를 위한 인공지능 AI <85654856+dscoool@users.noreply.github.com>
2025-03-06 10:09:33 +0900

    정보보안 streamlit 실습

---
+++ main29-3.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import datetime
+import pyupbit
+d = st.date_input( #날짜선택부분
+    "날짜를 선택하세요",
+    datetime.date.today())
+st.write('비트코인 1일 차트') #제목
+ticker = 'KRW-BTC' # 코인종류 지정
+interval = 'minute60' # 1시간마다 기록하는 데이터
+to = str(d + datetime.timedelta(days=1)) # ~까지의 데이터
+count = 24 # 1일, 24시간
+price_now = pyupbit.get_ohlcv(ticker=ticker, #가격 데이터 가져오기
+        interval=interval,to=to,count=count)
+st.line_chart(price_now.close) # 그래프 그리기
